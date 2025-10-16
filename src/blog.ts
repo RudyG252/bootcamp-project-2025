f10a853811c67bf95873f49348a1d0e848633e9a
RudyG252 <Rudyagood@gmail.com>
2025-10-16 14:27:37 -0700

    feat: finish writing blog 2

--- src/blog.ts
+++ src/blog.ts
@@ -9,34 +9,24 @@ type Blog = {
 
 const blogs: Blog[] = [
   {
-    title: "Blog3",
-    date: "date",
-    description: `A blog post is a single article or piece of content published on a blog, which is an
-     online platform for sharing information, thoughts, or ideas. These posts can cover a variety of 
-     topics and include text, images, and videos. They are typically displayed in reverse chronological order
-      on the blog, meaning the most recent post appears first.`,
-    image: "sky.jpg",
-    imageAlt: "imageAlt",
-    slug: "blog1",
-  },
-  {
-    title: "Blog2",
-    date: "date",
-    description: `A blog post is a single article or piece of content published on a blog, which is an
-     online platform for sharing information, thoughts, or ideas. These posts can cover a variety of 
-     topics and include text, images, and videos. They are typically displayed in reverse chronological order
-      on the blog, meaning the most recent post appears first.`,
-    image: "sky.jpg",
-    imageAlt: "imageAlt",
-    slug: "blog2",
+    title: "My Love of Rackets",
+    date: "10/16/25",
+    description: `I've played a ton of racket sports in my life, and loved or at least liked pretty much all of them,
+     so this blog post will be pretty much decicated to talking about my experiences in different racket
+     sports, how I got into them, how good/bad I am at them, and how I think they translate between 
+     one another.
+    `,
+    image: "./blogs/images/rackets.jpg",
+    imageAlt: "an image of a variety of different rackets",
+    slug: "My_Love_of_Rackets",
   },
   {
     title: "My First Blog Entry",
-    date: "10/16/25",
+    date: "10/15/25",
     description: `This is just a small entry for my first blog post, in which I will brainstorm and hopefully
      find ideas for future blog posts.`,
-    image: "sky.jpg",
-    imageAlt: "imageAlt",
+    image: "./blogs/images/brainstorm.jpg",
+    imageAlt: "A brain with lighting around it",
     slug: "First_Blog",
   },
 ];
